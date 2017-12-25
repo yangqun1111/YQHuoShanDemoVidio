29df824709105c2045274bb17aa1cd6f1416e4da
yangqun1111 <2799823356@qq.com>
2017-12-25 14:38:22 +0800

    第一次上传

---
+++ app/src/main/java/mvpframework/yangqun/com/yqhuoshandemovidio/MainActivity.java
@@ -0,0 +1,13 @@
+package mvpframework.yangqun.com.yqhuoshandemovidio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//1，这是新上传的项目
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
